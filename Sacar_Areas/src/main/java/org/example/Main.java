bf42e3fd9ae994dac3a3b3af9103ed713a047c65
JoshLex30 <alexis_josh30@hotmail.com>
2025-06-11 18:40:10 -0600

    Areas funcional

---
+++ Sacar_Areas/src/main/java/org/example/Main.java
@@ -0,0 +1,63 @@
+package org.example;
+
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Menu de opciones");
+        System.out.println("1.Area de un cuadrado \n" +
+                "2.Area de un triangulo \n" +
+                "3.Areas de un rectangulo \n" +
+                "4.Area de un circulo\n" +
+                "5.Salir");
+        System.out.println("Escoja una opcion:");
+        int op=sc.nextInt();
+
+        switch (op){
+            case 1:
+                System.out.println("Ingresa el lado del cuadrado");
+                int lado_cuadrado = 0 ;
+                lado_cuadrado = sc.nextInt();
+                int total_cuadrado = lado_cuadrado * lado_cuadrado;
+                System.out.println("El area total es: " + total_cuadrado);
+                break;
+            case 2:
+                System.out.println("Ingresa la base");
+                int base=sc.nextInt();
+                System.out.println("Ingresa la altura:");
+                int altura=sc.nextInt();
+                int total=(base*altura)/2;
+                System.out.println("El area total es: " + total);
+
+                break;
+            case 3:
+                System.out.println("Ingresa la base");
+                int base2=sc.nextInt();
+                System.out.println("Ingresa la altura:");
+                int altura2=sc.nextInt();
+                int total1=(base2*altura2);
+                System.out.println("El area total es: " + total1);
+                break;
+            case 4:
+                System.out.println("Ingresa el radio:");
+                int radio= sc.nextInt();
+                int r=(radio*radio);
+                float total3= (float) (3.1416*r);
+                System.out.println("El area total es:"  +total3);
+
+
+                break;
+            case 5:
+                System.out.println("Saliendo....");
+                break;
+
+            default:
+                System.out.println("Numero no valido");
+                break;
+        }
+
+        }
+    }
